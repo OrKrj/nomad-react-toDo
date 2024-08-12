088e73a1799abc70912770d83638919de6db3c37
Zun <k5orkrj@gmail.com>
2024-08-12 23:28:37 +0900

    feature for upadate category

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,41 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((todo) => todo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To DO</button>
-      <button>Done</button>
+      {category !== "Doing" && (
+        <button name="Doing" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "To_Do" && (
+        <button name="To_Do" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "Done" && (
+        <button name="Done" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
